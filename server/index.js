2e33c556b4df77cbfcfb707e64af1fc670521d44
Jemy19 <gellidojeremy@gmail.com>
2024-05-03 01:03:39 +0800

    added sever and client conn

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 
-const app = express()
+const app = express();
+
+app.use('/', require('./routes/authRoutes'))
 
 const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
