489b6189a152851f00066887986f148c074dc7cd
jemy <gellidojeremy@gmail.com>
2024-05-09 19:10:02 +0800

    added conn to db

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,37 @@
-
+const User = require('../models/user')
 
 const test = (req, res) => {
     res.json('test is working')
 }
 
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        if (!name){
+            return res.json({
+                error: 'name is requred'
+            })
+        }
+        if (!password || password.length < 6){
+            return res.json({
+                error: 'Password is require and should be 6 char long'
+            })
+        }
+        const exist = await User.findOne({email})
+        if(exist){
+            return res.json({
+                error: 'email is already taken'
+            })
+        }
+        const user  = await User.create ({
+            name, email, password
+        })
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+    }
+}
 module.exports =  {
-    test
+    test,
+    registerUser
 }
