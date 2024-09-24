61e88f46e20c08cba42a858709aa810cd7aba3e0
Jemy19 <gellidojeremy@gmail.com>
2024-09-24 19:17:55 +0800

    revert changes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Home';
-import Register from './pages/Register';
+import {Routes, Route} from 'react-router-dom'
+import Homepage from './pages/Home'
+import Register from './pages/Register'
 import axios from 'axios';
-import { Toaster } from 'react-hot-toast';
-import { UserContextProvider } from '../context/userContext';
-import Dashboard from './pages/Dashboard';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './pages/Login';
-import OldDashboard from './pages/Oldashboard';
+import {Toaster} from 'react-hot-toast'
+import { UserContextProvider } from '../context/userContext'
+import Dashboard from './pages/Dashboard'
+import PrivateRoute from './components/PrivateRoute'
+import Login from './pages/Login'
+import OldDashboard from './pages/Oldashboard'
 import Management from './pages/Management';
 import PublicRoute from './components/PublicRoute';
 import Vidtest from './pages/vidtest';
@@ -20,38 +20,17 @@ import SASignManagement from './pages/superAdmin/saSignManagement';
 import SAFeedbacks from './pages/superAdmin/saFeedback';
 import SALogs from './pages/superAdmin/saLogs';
 import Notfound from './pages/404NotFound/404';
+
 import UserManagement from './pages/UserManagement';
 import AdminLogs from './pages/adminLogs';
 
-// Set the base URL and credentials for Axios
 axios.defaults.baseURL = 'https://kumpas.onrender.com';
-axios.defaults.withCredentials = true;
-
-// Default favicon and loading spinner paths
-const defaultFavicon = '/src/assets/Logo.svg'; // Original logo
-const loadingSpinner = '/src/assets/logospin.svg'; // Path to your spinner SVG
-
-// Add Axios interceptors for request and response
-axios.interceptors.request.use((config) => {
-  document.getElementById('favicon').href = loadingSpinner; // Change to spinner favicon
-  return config;
-}, (error) => {
-  document.getElementById('favicon').href = defaultFavicon; // Revert on error
-  return Promise.reject(error);
-});
-
-axios.interceptors.response.use((response) => {
-  document.getElementById('favicon').href = defaultFavicon; // Revert on success
-  return response;
-}, (error) => {
-  document.getElementById('favicon').href = defaultFavicon; // Revert on error
-  return Promise.reject(error);
-});
+axios.defaults.withCredentials = true
 
 function App() {
   return (
     <UserContextProvider>
-      <Toaster position='top-center' toastOptions={{ duration: 4000 }} />
+      <Toaster position = 'top-center' toastOptions={{duration: 4000}}/>
       <Routes>
         <Route path='*' element={<Notfound />} />
         <Route path='/' element={<Homepage />} />
@@ -71,8 +50,8 @@ function App() {
         <Route path='/SAFeedbacks' element={<SuperAdminRoute><SAFeedbacks /></SuperAdminRoute>} />
         <Route path='/SALogs' element={<SuperAdminRoute><SALogs /></SuperAdminRoute>} />
       </Routes>
-    </UserContextProvider>
-  );
+    </UserContextProvider>  
+  )
 }
 
-export default App;
+export default App
