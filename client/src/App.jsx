b806c1adba5dc07571e89bb3d1b1d168090a1b89
Jemy19 <gellidojeremy@gmail.com>
2024-10-03 12:35:53 +0800

    delete old paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import {Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Home'
-import Register from './pages/Register'
 import axios from 'axios';
 import {Toaster} from 'react-hot-toast'
 import { UserContextProvider } from '../context/userContext'
 import Dashboard from './pages/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
 import Login from './pages/Login'
-import OldDashboard from './pages/Oldashboard'
 import Management from './pages/Management';
 import PublicRoute from './components/PublicRoute';
 import Vidtest from './pages/vidtest';
