892a5397c30eacd79ac0f8bdabf19270afc54828
Jemy19 <gellidojeremy@gmail.com>
2024-10-03 19:52:26 +0800

    minor fix on login

--- client/src/components/NavbarLog.jsx
+++ client/src/components/NavbarLog.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link to="/login">
-          <img src={Logo} alt="Logo" className="w-auto" style={{ height: '150px' }}/>
+        <img src={Logo} alt="Logo" className="w-auto h-10" />
         </Link>
         {user ? (
           <>
