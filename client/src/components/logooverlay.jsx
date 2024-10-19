2535050c4320b7c357ba5e61269c582193e00088
Jemy19 <gellidojeremy@gmail.com>
2024-10-19 19:02:44 +0800

    added logo overlay

--- client/src/components/logooverlay.jsx
+++ client/src/components/logooverlay.jsx
@@ -4,7 +4,7 @@ const LogoOverlay = () => {
   return (
     <div className="fixed top-0 left-0 p-4 z-50">
       <a href="/">
-        <img src={logoname} alt="Logo" className="h-20 w-20" />
+        <img src={logoname} alt="Logo" className="h-40 w-40" />
       </a>
     </div>
   );
