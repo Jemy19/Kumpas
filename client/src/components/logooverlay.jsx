6b76f91af6dc49eda106550829a2754682e23693
Jemy19 <gellidojeremy@gmail.com>
2024-10-19 19:05:12 +0800

    added logo overlay

--- client/src/components/logooverlay.jsx
+++ client/src/components/logooverlay.jsx
@@ -2,7 +2,7 @@ import logoname from '@/assets/logoname.svg'
 
 const LogoOverlay = () => {
   return (
-    <div className="fixed top-0 left-0 p-4 z-50">
+    <div className="fixed top-0 left-0z-50">
       <a href="/">
         <img src={logoname} alt="Logo" className="h-40 w-40" />
       </a>
